773fac0908bf3686763f1a48502828a3032754ff
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-05-24 20:12:10 +0700

    (feat): create url-parser for managing url for pages

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,53 +8,17 @@ import "../styles/main.scss";
 
 import App from './views/app'
 
-import data from "../DATA.json";
-
-const drawerElement = document.querySelector("#drawer");
-const menuElement = document.querySelector("#menu");
-const mainElement = document.querySelector("main");
-
 const app = new App({
   button: document.querySelector("#menu"),
   drawer: document.querySelector("#drawer"),
   content: document.querySelector("main"),
 })
 
-data.restaurants.forEach((restaurant) => {
-  document.querySelector(".restaurant__list").innerHTML += `
-    <div class="restaurant__item">
-
-    <img
-        src="${restaurant.pictureId}"
-        alt="${restaurant.name}     "
-        class="restaurant__item--img"
-    />
-    <div class="restaurant__item--title">
-        <a href="#">${restaurant.name}</a>
-        <p>${restaurant.city}</p>
-    </div>
-    <div class="restaurant__item--content">
-        <p class="desc">
-        ${restaurant.description}
-        </p>
-        <p class="rating">
-        <span class="fa fa-star checked"></span>
-        ${restaurant.rating}
-        </p>
-    </div>
-    </div>`;
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+ 
+window.addEventListener('load', () => {
+  app.renderPage();
 });
 
-if (screen.width >= 850) {
-  var prevScrollpos = window.pageYOffset;
-  document.getElementById("drawer").style.top = "-10rem";
-  window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      document.getElementById("drawer").style.top = "0";
-    } else {
-      document.getElementById("drawer").style.top = "-10rem";
-    }
-    prevScrollpos = currentScrollPos;
-  };
-}
---
+++ src/scripts/views/pages/home.js
@@ -0,0 +1,45 @@
+import data from "../../../DATA.json";
+
+const Home = {
+    async render() {
+      return `
+        <hero-section></hero-section>
+
+        <specialty-section></specialty-section>
+
+        <section class="restaurant">
+            <h2 class="restaurant__title">our list of restaurant</h2>
+            <div class="restaurant__list"></div>
+        </section>
+      `;
+    },
+   
+    async afterRender() {
+        data.restaurants.forEach((restaurant) => {
+            document.querySelector(".restaurant__list").innerHTML += `
+              <div class="restaurant__item">
+          
+              <img
+                  src="${restaurant.pictureId}"
+                  alt="${restaurant.name}     "
+                  class="restaurant__item--img"
+              />
+              <div class="restaurant__item--title">
+                  <a href="#/detail/${restaurant.id}">${restaurant.name}</a>
+                  <p>${restaurant.city}</p>
+              </div>
+              <div class="restaurant__item--content">
+                  <p class="desc">
+                  ${restaurant.description}
+                  </p>
+                  <p class="rating">
+                  <span class="fa fa-star checked"></span>
+                  ${restaurant.rating}
+                  </p>
+              </div>
+              </div>`;
+          });
+    },
+  };
+   
+  export default Home;
