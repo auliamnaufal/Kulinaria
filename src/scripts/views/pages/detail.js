4a6473c7452d1e3839b46d26a6bab2c40244f593
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-05-26 21:06:17 +0700

    (perf, refactor): move all display detail restauran to template-creator.js

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,6 @@
 import KulinariaDataSource from '../../data/dataSource';
 import UrlParser from '../../routes/url-parser';
-import {
-  createRestaurantDetailTemplate,
-  createMenuListTemplate,
-  createCategoryListTemplate,
-  createReviewListTemplate,
-} from '../templates/template-creator';
+import { createRestaurantDetailTemplate } from '../templates/template-creator';
 import Scroll from '../../utils/scroll';
 import LikeButtonInitiator from '../../utils/like-button-initiator';
 
@@ -14,53 +9,7 @@ const Detail = {
     return `
       <div id="detail"></div>
 
-      <section class="category">
-        <div class="category container">
-          <h2 class="category__heading">Kategori Restoran</h2>
-          <div class="category__list">
-              
-          </div>
-        </div>
-      </section>
       
-      <section id="menulist">
-        <div class="menulist container">
-          <div>
-              <h2>Daftar Makanan</h2>
-              <ul class="foods"></ul>
-          </div>
-
-          <div>
-              <h2>Daftar Minuman</h2>
-              <ul class="drinks"></ul>
-          </div>
-        </div>
-      </section>
-        
-      <section id="review">
-        <div class="review container">
-          <h2 class="review__heading">Ulasan Restoran</h2>
-          <div class="review__list container"></div>
-        </div>
-      </section>
-
-      <section class="form">
-        <h2 class="container form__heading">Tambahkan Ulasan</h2>
-        <div class="form">
-          <label for="name">Nama:</label><br />
-          <input maxlength="25" type="text" id="name" name="fname" /><br />
-          <label for="review">Pendapat:</label><br />
-          <textarea
-            maxlength="90"
-            type="text"
-            id="reviews"
-            name="review"
-          ></textarea>
-          <div class="card-btn">
-            <button id="submit"22>Kirim</button>
-          </div>
-        </div>
-      </section>
 
       <div id="likeButtonContainer"></div>
     `;
@@ -73,28 +22,8 @@ const Detail = {
     const restaurant = await KulinariaDataSource.detailResto(url.id);
 
     const restaurantContainer = document.querySelector('#detail');
-    const foodContainer = document.querySelector('.foods');
-    const drinkContainer = document.querySelector('.drinks');
-    const categoryContainer = document.querySelector('.category__list');
-    const reviewContainer = document.querySelector('.review__list');
 
-    restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
-
-    restaurant.restaurant.menus.foods.forEach((food) => {
-      foodContainer.innerHTML += createMenuListTemplate(food);
-    });
-
-    restaurant.restaurant.menus.drinks.forEach((drink) => {
-      drinkContainer.innerHTML += createMenuListTemplate(drink);
-    });
-
-    restaurant.restaurant.categories.forEach((category) => {
-      categoryContainer.innerHTML += createCategoryListTemplate(category);
-    });
-
-    restaurant.restaurant.customerReviews.forEach((review) => {
-      reviewContainer.innerHTML += createReviewListTemplate(review);
-    });
+    restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant.restaurant);
 
     LikeButtonInitiator.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -2,65 +2,118 @@ import CONFIG from '../../globals/config';
 
 const createRestaurantDetailTemplate = (resto) => `
     <section id="hero">
-    <img
-    src="${CONFIG.BASE_URL + CONFIG.IMAGE_URL_MED + resto.restaurant.pictureId}"
-    alt="${resto.restaurant.name}"
-    class="detail__img"
-    />
-    <h2 class="detail__heading container">${resto.restaurant.name}</h2>
+        <img
+            src="${CONFIG.BASE_URL + CONFIG.IMAGE_URL_MED + resto.pictureId}"
+            alt="${resto.name}"
+            class="detail__img"
+        />
+        <h2 class="detail__heading container">${resto.name}</h2>
     </section>
 
     <section id="info"  class="container">
-    <div class="detail__info">
-    <div class="detail__info__rating">
-        <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        >
-        <path
-            d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
-        />
-        </svg>
-        <p>${resto.restaurant.rating}</p>
-    </div>
-    <div class="detail__info__location">
-        <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        >
-        <path
-            d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"
-        />
-        </svg>
-        <p>${resto.restaurant.address}, ${resto.restaurant.city}</p>
-    </div>
-    <p class="detail__info__paraghraph">
-        ${resto.restaurant.description}
-    </p>
-    </div>
+        <div class="detail__info">
+            <div class="detail__info__rating">
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                >
+                <path
+                    d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"
+                />
+                </svg>
+                <p>${resto.rating}</p>
+            </div>
+            <div class="detail__info__location">
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                >
+                <path
+                    d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"
+                />
+                </svg>
+                <p>${resto.address}, ${resto.city}</p>
+            </div>
+            <p class="detail__info__paraghraph">
+                ${resto.description}
+            </p>
+        </div>
     </section>
-`;
 
-const createMenuListTemplate = (menu) => `
-    <li>${menu.name}</li>
-`;
+    <section class="category">
+        <div class="category container">
+            <h2 class="category__heading">Kategori Restoran</h2>
+            <div class="category__list">
+                ${resto.categories.map((category) => `
+                    <p>${category.name}</p> 
+                `).join('')}
+            </div>
+        </div>
+    </section>
 
-const createCategoryListTemplate = (category) => `
-    <p>${category.name}</p> 
-`;
+    <section id="menulist">
+        <div class="menulist container">
+        <div>
+            <h2>Daftar Makanan</h2>
+            <ul class="foods">
+                ${resto.menus.foods.map((food) => `
+                    <li>${food.name}</li> 
+                `).join('')}
+                
+            </ul>
+        </div>
 
-const createReviewListTemplate = (review) => `
-    <div class="review__item">
-        <h3 class="review__item__name">${review.name}</h3>
-        <p class="review__item__review">
-            <q>${review.review}</q>
-        </p>
-        <p class="review__item__date">${review.date}</p>
-    </div>
+        <div>
+            <h2>Daftar Minuman</h2>
+            <ul class="drinks">
+                ${resto.menus.drinks.map((drink) => `
+                    <li>${drink.name}</li>
+                `).join('')}
+                
+            </ul>
+        </div>
+        </div>
+    </section>
+
+    <section id="review">
+        <div class="review container">
+            <h2 class="review__heading">Ulasan Restoran</h2>
+            <div class="review__list container">
+                ${resto.customerReviews.map((review) => `
+                    <div class="review__item">
+                        <h3 class="review__item__name">${review.name}</h3>
+                        <p class="review__item__review">
+                            <q>${review.review}</q>
+                        </p>
+                        <p class="review__item__date">${review.date}</p>
+                    </div>
+                `).join('')}
+            </div>
+        </div>
+    </section>
+
+    <section class="form">
+        <h2 class="container form__heading">Tambahkan Ulasan</h2>
+        <div class="form">
+            <label for="name">Nama:</label><br />
+            <input maxlength="25" type="text" id="name" name="fname" /><br />
+            <label for="review">Pendapat:</label><br />
+            <textarea
+                maxlength="90"
+                type="text"
+                id="reviews"
+                name="review"
+            ></textarea>
+            <div class="card-btn">
+                <button id="submit"22>Kirim</button>
+            </div>
+        </div>
+    </section>
+    
 `;
 
 const createRestoItemTemplate = (list) => `
@@ -102,9 +155,6 @@ const createLikedButtonTemplate = () => `
 export {
   createRestoItemTemplate,
   createRestaurantDetailTemplate,
-  createMenuListTemplate,
-  createCategoryListTemplate,
-  createReviewListTemplate,
   createLikeButtonTemplate,
   createLikedButtonTemplate,
 };
