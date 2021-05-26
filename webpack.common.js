8eeff73503f264369e3898110203b9069d5fbd91
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-05-26 12:27:01 +0700

    (test): tested webpackPwaManifest for generating manifest.json and injected it to index.html

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+
 const path = require('path');
 
 module.exports = {
@@ -48,8 +49,27 @@ module.exports = {
         },
       ],
     }),
-    new FaviconsWebpackPlugin({
-      logo: path.resolve(__dirname, 'src/public/images/KulinariaFavicon.png'),
+    new WebpackPwaManifest({
+      name: 'Kulinaria',
+      short_name: 'Kulinaria',
+      description: 'My awesome Progressive Web App!',
+      background_color: '#ffffff',
+      crossorigin: 'use-credentials',
+      icons: [
+        {
+          src: path.resolve('src/public/images/KulinariaFavicon.png'),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+        },
+        {
+          src: path.resolve('src/public/images/KulinariaFavicon.png'),
+          size: '1024x1024', // you can also use the specifications pattern
+        },
+        {
+          src: path.resolve('src/public/images/KulinariaFavicon.png'),
+          size: '1024x1024',
+          purpose: 'maskable',
+        },
+      ],
     }),
   ],
 };
