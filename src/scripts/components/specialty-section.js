b205ead9c551472508c982c9ab2d872ceef8c8ef
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-06-16 21:36:14 +0700

    (perf): deleted font awesome cdn request

--- src/scripts/components/specialty-section.js
+++ src/scripts/components/specialty-section.js
@@ -10,17 +10,17 @@ class SpecialtySection extends HTMLElement {
                 <h2 class="specialty__content--title">spesialisasi kami</h2>
                 <div class="gallery">
                     <div class="gallery__item">
-                        <i class="fas fa-user-check icon"></i>
+                        <img src="./public/icon/chef gear hat 1.svg" alt="best chef" class="icon">
                         <p class="gallery__item--title">Koki Terbaik</p>
                     </div>
 
                     <div class="gallery__item">
-                        <i class="fas fa-home icon"></i>
+                        <img src="./public/icon/house chimney 1.svg" alt="convinient place" class="icon">
                         <p class="gallery__item--title" tabindex="0">Tempat Yang Nyaman</p>
                     </div>
 
                     <div class="gallery__item">
-                        <i class="fas fa-carrot icon"></i>
+                        <img src="./public/icon/vegetables broccoli.svg" alt="organic food" class="icon">
                         <p class="gallery__item--title">Makanan Organik</p>
                     </div>
                 </div>
