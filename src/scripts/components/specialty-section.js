173e3503ef7ce17cc86df4809d6aa640fe915207
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-06-16 21:47:07 +0700

    (perf): added lazyloading for icon

--- src/scripts/components/specialty-section.js
+++ src/scripts/components/specialty-section.js
@@ -10,17 +10,17 @@ class SpecialtySection extends HTMLElement {
                 <h2 class="specialty__content--title">spesialisasi kami</h2>
                 <div class="gallery">
                     <div class="gallery__item">
-                        <img src="./public/icon/chef gear hat 1.svg" alt="best chef" class="icon">
+                        <img data-src="./public/icon/chef gear hat 1.svg" alt="best chef" class="icon lazyload">
                         <p class="gallery__item--title">Koki Terbaik</p>
                     </div>
 
                     <div class="gallery__item">
-                        <img src="./public/icon/house chimney 1.svg" alt="convinient place" class="icon">
+                        <img data-src="./public/icon/house chimney 1.svg" alt="convinient place" class="icon lazyload">
                         <p class="gallery__item--title" tabindex="0">Tempat Yang Nyaman</p>
                     </div>
 
                     <div class="gallery__item">
-                        <img src="./public/icon/vegetables broccoli.svg" alt="organic food" class="icon">
+                        <img data-src="./public/icon/vegetables broccoli.svg" alt="organic food" class="icon lazyload">
                         <p class="gallery__item--title">Makanan Organik</p>
                     </div>
                 </div>
