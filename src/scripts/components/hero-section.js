96ea7c32edafee4d98bca954b8069ea4c8367e2e
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-06-16 14:14:31 +0700

    (test): try adding responsive image to hero

--- src/scripts/components/hero-section.js
+++ src/scripts/components/hero-section.js
@@ -6,6 +6,13 @@ class HeroSection extends HTMLElement {
   render() {
     this.innerHTML = `
         <div class="hero">
+        <picture>
+                <source media="(max-width: 600px)" srcset="./public/images/hero-image_2.jpg">
+                <img 
+                    src='./public/images/hero-image_2.jpg' 
+                    alt="Hero image"></img>
+            
+            </picture>
             <div class="hero__content">
                 <h1 class="hero__content--title">Kulinaria</h1>
                 <p class="hero__content--text">Tempat di mana Anda menemukan keceriaan</p>
