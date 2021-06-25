9616a88413ca4cb1d1fcd8ed09991921d5a90796
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-06-25 13:31:26 +0700

    (test): delete unnecessary test code & move user review to a new file

---
+++ e2e/Adding_User_Review.spec.js
@@ -0,0 +1,30 @@
+Feature('Adding user review')	
+const assert = require('assert')
+
+Scenario('User Restaurant Review', async ({ I }) => {
+
+	I.amOnPage('#/')
+	I.seeElement('.specialty')
+	
+	I.seeElement('.skeleton');
+	I.seeElement('.item')
+
+	I.click(locate('.restaurant__item--title a').first())
+
+	I.seeElement('.form')
+
+	
+	const userReview = 'mantap mantul oke siap mantap'
+	I.fillField('#name', 'Naufal Aulia')	
+
+	I.fillField('#reviews', userReview)
+	I.click('#submit')
+
+	I.see(userReview, '.review__item__review q');
+
+	const lastReview = locate('.review__item__review q').last();
+	const lastReviewText = await I.grabTextFrom(lastReview)
+
+	assert.strictEqual(userReview, lastReviewText)
+	
+})
--- e2e/Liking_Movies.spec.js
+++ e2e/Liking_Movies.spec.js
@@ -59,32 +59,3 @@ Scenario('Unsave restaurant', async ({ I }) => {
 	I.amOnPage('#/favorite')
 	I.see('Kamu Tidak Memiliki Restoran Favorit, bisa Tambah Disini', '.nofavorite');
 })
-
-Scenario('User Restaurant Review', async ({ I }) => {
-	I.see('Kamu Tidak Memiliki Restoran Favorit, bisa Tambah Disini', '.nofavorite');
-
-	I.amOnPage('#/')
-	I.seeElement('.specialty')
-	
-	I.seeElement('.skeleton');
-	I.seeElement('.item')
-
-	I.click(locate('.restaurant__item--title a').first())
-
-	I.seeElement('.form')
-
-	
-	const userReview = 'mantap mantul oke siap mantap'
-	I.fillField('#name', 'Naufal Aulia')	
-
-	I.fillField('#reviews', userReview)
-	I.click('#submit')
-
-	I.see(userReview, '.review__item__review q');
-
-	const lastReview = locate('.review__item__review q').last();
-	const lastReviewText = await I.grabTextFrom(lastReview)
-
-	assert.strictEqual(userReview, lastReviewText)
-	
-})
