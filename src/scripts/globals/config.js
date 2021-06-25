bfa0f6ec8b0c0920cd87441052aab655d038f90a
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-06-25 13:35:48 +0700

    (build): changed cache name prefix to app name

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   BASE_URL: 'https://dicoding-restaurant-api.el.r.appspot.com/',
   IMAGE_URL_MED: 'images/medium/',
   IMAGE_URL_SML: 'images/small/',
-  CACHE_NAME_PREFIX: new Date().toISOString(),
+  CACHE_NAME_PREFIX: 'kulinaria-cache',
   DATABASE_NAME: 'kulinaria-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurant',
